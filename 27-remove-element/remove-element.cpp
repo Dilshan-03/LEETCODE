01336570b4be10e337808f76d97a1b5c37976dd7
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-20 22:35:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (43.94%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,17 +4,21 @@ public:
         int n = nums.size();
         int last = n - 1;
         int front = 0;
+        //last is the pointer , which points to non - val elements
         while(front <= last){
+            //If the val is occurred both at the front and back too , we can just reduce the last pointer by 1
             if(nums[front] == val && nums[last] == val){
                 last--;
                 continue;
             }
+            //If the val is occurred only at the front , we can just swap it with last 
             else if(nums[front] == val){
                 swap(nums[front] , nums[last]);
                 last--;
             }
             front++;
         }
+        //For the cnt , we can return last + 1
         return last + 1;
     }
 };
