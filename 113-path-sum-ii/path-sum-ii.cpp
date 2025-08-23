b74b33c247eafab771e802a79e88eef742201182
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-23 22:48:56 +0530

    Time: 3 ms (48.21%) | Memory: 21 MB (46.51%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool isLeaf(TreeNode* root){
+        return (root->left == NULL && root->right == NULL);
+    }
+    void addValidPath(TreeNode* root , vector<int>& path , vector<vector<int>>& ans , int targetSum){
+        if(root == NULL) return;
+        if(isLeaf(root)){
+            if(targetSum == root->val){
+                path.push_back(root->val);
+                ans.push_back(path);
+                path.pop_back();
+            }
+            return;
+        }
+        path.push_back(root->val);
+        addValidPath(root->left , path , ans , targetSum - root->val);
+        addValidPath(root->right , path , ans , targetSum - root->val);
+        path.pop_back();
+    }
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        addValidPath(root , path , ans , targetSum);
+        return ans;
+    }
+};
