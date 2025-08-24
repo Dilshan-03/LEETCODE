78decf177be221938e6f7fa01a882e24141f9298
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-24 23:11:24 +0530

    Time: 15 ms (29.41%) | Memory: 32 MB (55.15%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -4,6 +4,7 @@ public:
         sort(nums.begin() , nums.end());
         int n = nums.size();
         int sum = 0;
+        //Just grp the closer elements together , just a greedy way for maximising the answer
         for(int i = 0 ; i < n; i += 2){
             sum += nums[i];
         }
