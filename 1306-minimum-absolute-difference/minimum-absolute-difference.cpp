08680645da0d738f8a9f745fa028143774d6da03
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-24 22:59:59 +0530

    Time: 15 ms (72.96%) | Memory: 36.6 MB (66.00%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -3,12 +3,13 @@ public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         sort(arr.begin() , arr.end());
         int n = arr.size();
-        vector<vector<int>> ans;
+        //Finding out the minimum Absolute Difference
         int mini = 1e9;
         for(int i = 1 ; i < n ; i++){
             mini = min(mini , abs(arr[i] - arr[i - 1]));
         }
-
+        //Store all the pairs which have minimum absolute difference
+        vector<vector<int>> ans;
         for(int i = 1 ; i < n ; i++){
             if(abs(arr[i] - arr[i - 1]) == mini){
                 ans.push_back({arr[i - 1] , arr[i]});
