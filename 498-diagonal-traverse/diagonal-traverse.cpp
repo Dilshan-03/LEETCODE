57a3e77c44b4ec9892c0909d62df74ed57d2631d
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-25 16:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (81.55%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -7,17 +7,21 @@ public:
         bool left_to_right = true;
         while(row < n && col < m){
             if(left_to_right){
+                // 2 cases for out of boundary while going from left to right 
                 while(true){
                     ans.push_back(mat[row][col]);
+                     // If right element is there , then just jump to it or else move to next row and break
                     if(row  == 0){
                         if(col + 1 < m) col = col + 1;
                         else row = row + 1;
                         break;
                     }
+                     // If ur in last col , then anyway we have to move to next row and break
                     else if(col + 1 == m){
                         row = row + 1;
                         break;
                     }
+                    //Just the move the pointers accordingly for left to right direction movement
                     row--;
                     col++;
                 }
@@ -26,20 +30,24 @@ public:
             else{
                 while(true){
                     ans.push_back(mat[row][col]);
+                    // If  element is there at the down  , then just jump to it or else move to next col and break
                     if(col == 0){
                         if(row + 1 < n) row = row + 1;
                         else col = col + 1;
                         break;
                     }
+                    // If ur in last row , then anyway we have to move to next col and break
                     else if(row + 1 == n){
                         col = col + 1;
                         break;
                     }
+                    //Just the move the pointers accordingly for right to left direction movement
                     row++;
                     col--;
                 }
                
             }
+            //Just flip the flag everytime
             left_to_right = !(left_to_right);   
         }
         return ans;
