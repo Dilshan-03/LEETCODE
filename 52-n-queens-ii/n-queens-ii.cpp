d30754ad429cd027a3107d01e819f3973e7bc962
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-25 17:12:26 +0530

    Time: 1 ms (68.18%) | Memory: 8.5 MB (38.63%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 private:
-    void backtrack(int col , vector<int>& leftRow , vector<int>& leftDiagonal , vector<int>& rightDiagonal , vector<string>& board , int n , int& cnt){
-        if(col == n){
-            cnt++;
-            return;
-        }
+    int backtrack(int col , vector<int>& leftRow , vector<int>& leftDiagonal , vector<int>& rightDiagonal , vector<string>& board , int n){
+        if(col == n)  return 1;
         
+        int cnt = 0;
         for(int row = 0 ; row < n ; row++){
             if(board[row][col] == '.' && !leftRow[row] && !leftDiagonal[n - 1 + col - row] && !rightDiagonal[row + col]){
                 board[row][col] = 'Q';
                 leftRow[row] = 1;
                 leftDiagonal[n - 1 + col - row] = 1;
                 rightDiagonal[row + col] = 1;
-                backtrack(col + 1 , leftRow , leftDiagonal , rightDiagonal , board , n , cnt);
+                cnt += backtrack(col + 1 , leftRow , leftDiagonal , rightDiagonal , board , n);
                 board[row][col] = '.';
                 leftRow[row] = 0;
                 leftDiagonal[n - 1 + col - row] = 0;
                 rightDiagonal[row + col] = 0;
             }
         }
+        return cnt;
 
     }
 public:
@@ -27,8 +26,6 @@ public:
         vector<int> leftDiagonal(2 * n - 1 , 0) , rightDiagonal(2 * n - 1 , 0);
         string temp(n , '.');
         vector<string> board(n , temp);
-        int cnt = 0;
-        backtrack(0 , leftRow , leftDiagonal , rightDiagonal , board , n , cnt);
-        return cnt;
+        return backtrack(0 , leftRow , leftDiagonal , rightDiagonal , board , n);
     }
 };
