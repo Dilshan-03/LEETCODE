85946a709b7840e91ac8abd0c8b1b45a22341769
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-26 09:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 29.2 MB (77.05%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
         int n = dimensions.size();
-        int area = 0 , maxi = 0;
+        int area = 0 , maxDiagonal = 0;
         for(int i = 0 ; i < n ; i++){
             int curLen = dimensions[i][0];
             int curWidth = dimensions[i][1];
             //Finding out the hyptonese and comparing with maximum value everytime, just ignoring the sqrt part for simplicity
-            int diagonal = curLen * curLen + curWidth * curWidth;
-            if(diagonal > maxi){
-                maxi = diagonal;
+            int diagonal = (curLen * curLen) + (curWidth * curWidth);
+            //If there is a larger diagonal available , then calculate the area of that diagonal
+            if(diagonal > maxDiagonal){
+                maxDiagonal = diagonal;
                 area = curLen * curWidth;
             }
-            else if(diagonal == maxi) area = max(area ,curLen * curWidth);
+            //If there is anyother diagonal having same length as maxDiagonal then , update the area with maximum
+            else if(diagonal == maxDiagonal) area = max(area ,curLen * curWidth);
         }
         return area;
     }
