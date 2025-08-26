aa8fbc5c5b6f5aeefd542fb614387c76334e20de
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-26 19:18:31 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (88.67%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -4,17 +4,21 @@ public:
        int n = nums.size();
        int len = 0 , maxLen = 0;
        bool isIncrease = true;
-       //check for strictly increasing
        for(int i = 0 ; i < n - 1 ; i++){
             if(nums[i] < nums[i + 1]){
+                //If already in increasing half  , then just update the len by 1
                 if(isIncrease) len += 1;
+                //if not already in increasing half , then just put the len as 1 for the current element and make it as increasing
                 else{
                     len = 1;
                     isIncrease = true;
                 }
             } 
+
             else if(nums[i] > nums[i + 1]){
+                 //If already in decreasing half  , then just update the len by 1
                 if(!isIncrease) len += 1;
+                //if not already in decreasing half , then just put the len as 1 for the current element and make it as decreasing
                 else{
                     len = 1;
                     isIncrease = false;
@@ -23,7 +27,8 @@ public:
             else len = 0;
             maxLen = max(maxLen , len);
        } 
-   
+       
+       //To compensate for final element and initially taking len as 1
        return maxLen + 1;
 
     }
