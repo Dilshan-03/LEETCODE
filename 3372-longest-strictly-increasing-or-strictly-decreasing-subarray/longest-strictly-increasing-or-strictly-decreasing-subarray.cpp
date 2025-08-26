4a9c89e4d75d8fc7f4306233143f954a1089c028
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-26 19:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (61.59%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
        int n = nums.size();
-       int len = 1 , maxLen = 1;
+       int len = 0 , maxLen = 0;
+       bool isIncrease = true;
        //check for strictly increasing
-       for(int i = 1 ; i < n ; i++){
-            if(nums[i] > nums[i - 1]) len++;
-            else len = 1;
+       for(int i = 0 ; i < n - 1 ; i++){
+            if(nums[i] < nums[i + 1]){
+                if(isIncrease) len += 1;
+                else{
+                    len = 1;
+                    isIncrease = true;
+                }
+            } 
+            else if(nums[i] > nums[i + 1]){
+                if(!isIncrease) len += 1;
+                else{
+                    len = 1;
+                    isIncrease = false;
+                }
+            }
+            else len = 0;
             maxLen = max(maxLen , len);
        } 
-       //Check for strinctly decreasing
-       len = 1;
-       for(int i = 1 ; i < n ; i++){
-            if(nums[i] < nums[i - 1]) len++;
-            else len = 1;
-            maxLen = max(maxLen , len);
-       }   
-       return maxLen;
+   
+       return maxLen + 1;
 
     }
 };
