7acea26ba59258328bdbb68659e4a948f072903e
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-27 20:54:27 +0530

    Time: 15 ms (75.10%) | Memory: 93.4 MB (18.80%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -3,14 +3,14 @@ public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         int n = capacity.size();
         vector<int> needToFill;
-        for(int i = 0 ; i < n ; i++){
-            needToFill.push_back(capacity[i] - rocks[i]);;
-        }
-
+        for(int i = 0 ; i < n ; i++)
+            needToFill.push_back(capacity[i] - rocks[i]);
         sort(needToFill.begin() , needToFill.end());
         int cnt = 0;
         for(int i = 0 ; i < n ; i++){
+            //If it's already completed filled , then increment the count
             if(needToFill[i] == 0) cnt += 1;
+            //If the additional rock can be filled , then fill it up and increment the count and reduce the additional rocks accordingly
             else if(needToFill[i] <= additionalRocks){
                 additionalRocks -= needToFill[i];
                 cnt += 1;
