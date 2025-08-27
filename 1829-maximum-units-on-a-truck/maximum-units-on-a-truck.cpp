32480bd672611ef2ec182d007fd069d8c4efb24c
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-27 18:01:12 +0530

    Time: 11 ms (26.98%) | Memory: 20 MB (47.79%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -5,14 +5,17 @@ private:
     }
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        //sort the boxTypes according to no of units in a ascending order 
         sort(boxTypes.begin() , boxTypes.end() , comp);
 
         int n = boxTypes.size();
         int i = n - 1;
         int units = 0;
+        //Iterate from the end for maximum units
         while(i >= 0){
             int box = 0;
             if(truckSize == 0) break;
+            //If specific box in lesser than truck size than acquire them or else acquire only upto truckSize
             if(truckSize >= boxTypes[i][0]){
                 box = boxTypes[i][0];
                 truckSize -= box;
