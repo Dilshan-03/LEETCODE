8e580961d151bf11c476f855c50ba4d259a7b6d5
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-28 17:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (24.02%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> hash(26 , 0);
+        vector<int> last(26 , 0);
+        vector<int> ans;
         int n = s.length();
-        //
+        //Store the last occurance of the character
         for(int i = 0 ; i < n; i++){
-            hash[s[i] - 'a'] = i;
+            last[s[i] - 'a'] = i;
         }
         
         int maxLimit = -1;
         int start = 0;
-        vector<int> ans;
+      
+        //Greedy approach
         for(int i = 0 ; i < n ; i++){
-            maxLimit = max(maxLimit , hash[s[i] - 'a']);
+            //Similar to jump game , find the maxLimit you can reach 
+            maxLimit = max(maxLimit , last[s[i] - 'a']);
+            //If the maxLimit is equal to current index , then it means that you can partition that half and find the ans
             if(maxLimit == i){
                 ans.push_back(i - start + 1);
                 start = i + 1;
