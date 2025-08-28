76d395dbc7ab7943bdb1bd676cd3eb7c74d19b9d
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-28 18:00:41 +0530

    Time: 3 ms (86.98%) | Memory: 13 MB (91.50%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int partitionString(string s) {
-        vector<int> hash(26 , 0);
+        vector<int> lastSeen(26 , -1);
         int n = s.length();
         int partitionCnt = 1;
+        int currentSubstrStart = 0;
         for(int i = 0 ; i < n ; i++){
-            if(hash[s[i] - 'a'] != 0){
+            //Check whether the current char already appears within the current window , if so put a partition and change the partition start
+            //If the current character already appears but it's outside the current window , don't care about it , it's handled by the condition
+            if(lastSeen[s[i] - 'a'] >= currentSubstrStart){
                 partitionCnt += 1;
-                for(int i = 0 ; i < 26 ; i++) hash[i] = 0;   
+                currentSubstrStart = i;   
             }
-            hash[s[i] - 'a'] = 1;
+            lastSeen[s[i] - 'a'] = i;
         }
         return partitionCnt;
     }
