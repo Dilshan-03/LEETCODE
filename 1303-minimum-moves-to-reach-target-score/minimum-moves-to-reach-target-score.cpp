bc6d0cd40eb9024bca009c7edb4cee4ebfef1fc6
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-08-29 18:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (100.00%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
@@ -3,7 +3,6 @@ public:
     int minMoves(int target, int maxDoubles) {
         int steps = 0;
         while(target != 1 && maxDoubles != 0){
-            //if(maxDoubles == 0) steps += target - 1;
             if(target % 2 == 0){
                 target /= 2;
                 maxDoubles -= 1;
@@ -11,6 +10,8 @@ public:
             else target -= 1;
             steps += 1;
         }
+        //Whenever maxDoubles becomes zero , then steps for remaining target would be target - 1 
+        //If maxDoubles doesn't become zero , then anyway target becomes 1 so , target - 1 is nullified by itself
         return steps + target - 1;
     }
 };
