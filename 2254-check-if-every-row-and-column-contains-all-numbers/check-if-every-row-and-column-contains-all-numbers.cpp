4e869fd7eb4af15439011d4003d6bd52e8798d6f
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-09-04 21:56:17 +0530

    Time: 8 ms (73.33%) | Memory: 43.5 MB (64.68%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        //Checking for row
         for(int i = 0 ; i < n ; i++){
             vector<int> row(n + 1, 0);
             for(int j = 0 ; j < n ; j++){
                 int num = matrix[i][j];
-                if(num == 0 || num > n) return false;
-                if(row[num]) return false;
+                if(num == 0 || num > n || row[num]) return false;
                 row[num] =  true;
             }
         }
 
+        // Checking for column
         for(int j = 0 ; j < n ; j++){
             vector<int> col(n + 1, 0);
             for(int i = 0 ; i < n ; i++){
                 int num = matrix[i][j];
-                if(num == 0 || num > n) return false;
-                if(col[num]) return false;
+                if(num == 0 || num > n || col[num]) return false;
                 col[num] =  true;
             }
         }
