eefee030b4f870cc300a565993c2b8fdb5760da6
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-09-12 00:45:44 +0530

    Time: 0 ms (100.00%) | Memory: 32.8 MB (22.32%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -4,6 +4,7 @@ public:
         int n = num.size();
         vector<int> res;
         int carry = 0;
+        //Simulation of Normal arithmetic addition
         for(int i = n - 1 ; i >= 0 ; i--){
             int digit = k % 10;
             k = k / 10;
@@ -11,12 +12,14 @@ public:
             res.push_back(sum % 10);
             carry =  sum / 10;
         }
+        //If either given array ran out , but either of k or carry remains , do the same as above , by just ignoring num[i]
         k = k + carry;
         while(k){
             int sum = k % 10;
             res.push_back(sum);
             k = k / 10;
         }
+        //Since res stores answer in reverse order , reverse it up
         reverse(res.begin() , res.end());
         return res;
     }
