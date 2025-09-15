39039c89f528ba46b2edce9fd7656d22974c9332
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-09-15 15:04:12 +0530

    Time: 23 ms (54.33%) | Memory: 41.8 MB (78.85%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -3,13 +3,16 @@ public:
     int countCompleteSubarrays(vector<int>& nums) {
         int n = nums.size();
         unordered_set<int> st;
+        //Find the k - value ie the number of unique elements in the subarray
         for(int i = 0 ; i < n ; i++) st.insert(nums[i]);
         int k = st.size();
         int cnt = 0;
         unordered_map<int , int> mpp;
         int l = 0 , r = 0;
+        //Sliding window approach 
         while(r < n){
             mpp[nums[r]] += 1;
+            //As long as map size is k , cnt the subbarray possible with that configuration and reduce the size of the window
             while(mpp.size() == k){
                 cnt += (n - r);
                 mpp[nums[l]] -= 1;
