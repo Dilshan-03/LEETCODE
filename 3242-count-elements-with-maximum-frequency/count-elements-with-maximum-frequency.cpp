83d737039883ab68ec405f38c85a880481fd9de9
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-09-22 22:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (89.81%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -3,14 +3,18 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         unordered_map<int , int> freqMap;
         int maxFreq = 0;
+        int totalFreq = 0;
+        //Similar to that of finding second largest element approach
         for(auto it : nums){
             freqMap[it]++;
-            maxFreq = max(maxFreq , freqMap[it]);
+            int curFreq = freqMap[it];
+            if(curFreq > maxFreq){
+                maxFreq = curFreq;
+                totalFreq = curFreq;
+            }
+            else if(curFreq == maxFreq) totalFreq += curFreq;
         }
-        int cnt = 0;
-        for(auto it : freqMap){
-            if(it.second == maxFreq) cnt += it.second;
-        }
-        return cnt;
+        
+        return totalFreq;
     }
 };
