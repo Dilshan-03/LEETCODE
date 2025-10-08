1253530c20b3d83130eec4e59087688f924b5d8d
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-08 06:49:51 +0530

    Time: 39 ms (84.25%) | Memory: 128.4 MB (8.69%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,6 +1,8 @@
 class Solution {
 private:
     int findCnt(vector<int>& portions , long long success , int curSpell , int m){
+        //Lowerbound to find the first portion whose strength is greater than success 
+        //Since it's sorted , every portion to the left of that first portion also have greater strength than success
         int low = 0 , high = m - 1;
         while(low <= high){
             int mid = low + (high - low) / 2;
@@ -13,6 +15,7 @@ private:
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         int n = spells.size() , m = potions.size();
+        //Sort the potions
         sort(potions.begin() , potions.end());
         vector<int>cntPairs;
         for(int i = 0 ; i < n ; i++){
