ddea3f6255dae02e6384cf030805a815eab27da4
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-12 09:35:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (38.93%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -4,6 +4,7 @@ public:
 
         int n = colors.size();
         int ans = 0;
+        //Greedy approach -> anyway , Largest dist has to include either house at 0 th index or house at n - 1  th index
         for(int i = 1 ; i < n ; i++){
             if(colors[0] != colors[i]) ans = max(ans , i);
             if(colors[n - 1] != colors[i]) ans = max(ans , n - 1 - i);
