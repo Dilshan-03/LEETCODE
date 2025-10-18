a47846dcf98f5333bf4a911776d2ca8daf0dfe24
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-18 21:35:27 +0530

    Time: 25 ms (58.14%) | Memory: 86.6 MB (97.88%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -4,17 +4,19 @@ private:
         int cntPairs = 0;
         int i = 0;
         while(i < n){
+            //Greedy way , if the current adjacent pair  can have less than or equal maxDiff , then move don't take again that (i + 1)th index  
             if(i + 1 < n && nums[i + 1] - nums[i] <= maxDiff){
                 cntPairs += 1;
                 i += 1;
             }
+            //Regardless of anything , i has to move by 1 step
             i += 1;
         }
         return (cntPairs >= pairs);
     }
 public:
     int minimizeMax(vector<int>& nums, int p) {
-      
+        //Greedy way to get minimum difference
         sort(nums.begin() , nums.end());
         int n = nums.size();
         int low = 0 , high = nums[n - 1] - nums[0];
@@ -24,5 +26,7 @@ public:
             else low = mid + 1;
         }
         return low;
+        //TC : O(N log N) + O(N log (maxi - min))
+        //SC : O(1)
     }
 };
