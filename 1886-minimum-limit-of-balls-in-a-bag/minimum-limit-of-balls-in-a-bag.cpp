d090d432f63332e9cb726c0211967090ff5a14ee
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-18 19:04:23 +0530

    Time: 53 ms (16.01%) | Memory: 59.7 MB (78.02%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -2,8 +2,11 @@ class Solution {
 private:
     bool isPossible(vector<int>& nums , int maxOperations , int limit){
         int operations = 0;
+        //To calculate no of operations to maintain limit as max no of balls: we have to find no of bags needed to have limit as maximum no of balls for each element in the array
+        //Then (no of bags - 1) would be the no of operations for each element
         for(int i = 0 ; i < nums.size() ; i++){
-           operations += ceil((double)nums[i] / limit) - 1;
+           int bags = ceil((double) nums[i] / limit);
+           operations += bags - 1;
            if(operations > maxOperations) return false;
         }
         return (operations <= maxOperations);
