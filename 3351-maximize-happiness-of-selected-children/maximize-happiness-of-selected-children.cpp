1014dc48cc133a1822adde13f7579d2a6b5cf6b8
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-19 22:37:43 +0530

    Time: 156 ms (60.92%) | Memory: 107.9 MB (69.89%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
+        //Greedy approach , go for maxi happiness , every time
         sort(happiness.begin() , happiness.end());
         int n = happiness.size();
         long long maxHappiness = 0;
         int i = n - 1;
-        while(i >= 0 && k > 0 && happiness[i] >= (n - 1 - i)){
-            maxHappiness += (happiness[i] - (n - 1 - i));
+        //Since k <= n , don't have to check for i >= 0
+        while(k > 0){
+            maxHappiness += max(happiness[i] - (n - 1 - i) , 0);
             k -= 1;
             i -= 1;
         }
