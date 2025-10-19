8fd192164f85c72bb41868cb29ca649497a4b969
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-19 21:38:33 +0530

    Time: 27 ms (55.97%) | Memory: 88.4 MB (32.63%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,9 +1,9 @@
 class Solution {
 private:
     bool isPossible(vector<int>& candies , long long k , int maxCandy , int n){
-        if(!maxCandy) return true;
         long long childCnt = 0;
         for(int i = 0 ; i < n ; i++){
+            //Counting , how many children can have current piles of candies
             childCnt += (candies[i] / maxCandy);
         }
         return (childCnt >= k);
@@ -11,12 +11,15 @@ private:
 public:
     int maximumCandies(vector<int>& candies, long long k) {
         int n = candies.size();
-        int low = 0 , high = *max_element(candies.begin() , candies.end());
+        //technically low has to point to 0 , since 0 is also a answer . but shrinking down of answer space make sure that , high points to 0 , if it's the answer
+        int low = 1 , high = *max_element(candies.begin() , candies.end());
         while(low <= high){
             int mid = low + (high - low) / 2;
             if(isPossible(candies , k , mid , n)) low = mid + 1;
             else high = mid - 1;
         }
         return high;
+        //TC : O(N log (max_element))
+        //SC : O(1)
     }
 };
