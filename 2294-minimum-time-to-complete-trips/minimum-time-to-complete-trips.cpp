765ccbe544c1a349dfd2e264684d2d43b42afa80
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-19 22:24:02 +0530

    Time: 54 ms (60.00%) | Memory: 98.6 MB (16.15%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -3,15 +3,16 @@ private:
     bool isPossible(vector<int>& time , int totalTrips , long long t , int n){
         int trips = 0;
         for(int i = 0 ; i < n ; i++){
-            trips += (t /(long long)time[i]);
-            if(trips >= totalTrips) break;
+            trips += (t / time[i]);
+            if(trips >= totalTrips) return true;;
         }
-        return (trips >= totalTrips);
+        return false;
     }
 public:
     long long minimumTime(vector<int>& time, int totalTrips) {
         int n = time.size();
         int mini = *min_element(time.begin() , time.end());
+        //Max-Range : Bus with minimum time alone completes total no of trips
         long long low = 1 , high = 1LL * mini * totalTrips;
         while(low <= high){
             long long mid = low + (high - low) / 2;
