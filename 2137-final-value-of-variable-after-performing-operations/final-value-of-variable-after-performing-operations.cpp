51bcd530b4eaa319a60fc6051b56cc83150b5750
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-20 21:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (74.44%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -3,6 +3,7 @@ public:
     int finalValueAfterOperations(vector<string>& operations) {
         int val = 0;
         for(string it : operations){
+            //Anyway , middle index is always operator , regardless of pre or post
              if(it[1] == '+') val += 1;
              else val -= 1;
         }
