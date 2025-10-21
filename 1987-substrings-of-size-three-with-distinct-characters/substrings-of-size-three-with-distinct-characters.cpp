05e2987657dc4c91daaa4185138b729f35ac09a1
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-21 21:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (74.92%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -2,17 +2,9 @@ class Solution {
 public:
     int countGoodSubstrings(string s) {
         int n = s.length();
-        vector<int> hashMap(26 , 0);
-        int l = 0 , r = 0;
         int cnt = 0;
-        while(r < n){
-            hashMap[s[r] - 'a'] += 1;
-            while((r - l + 1 > 3) || (hashMap[s[r] - 'a'] > 1)){
-                hashMap[s[l] - 'a'] -= 1;
-                l += 1;
-            }
-            if(r - l + 1 == 3) cnt += 1;
-            r += 1;
+        for(int i = 0 ; i < n - 2 ; i++){
+            if(s[i] != s[i + 1] && s[i + 1] != s[i + 2] && s[i] != s[i + 2]) cnt += 1;
         }
         return cnt;
     }
