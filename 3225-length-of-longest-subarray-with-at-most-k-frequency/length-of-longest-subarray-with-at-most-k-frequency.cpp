bd7726e905e6780581b8aa2c9c249cc1d6c5a72e
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-22 09:33:58 +0530

    Time: 63 ms (62.91%) | Memory: 149.3 MB (80.67%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -3,20 +3,24 @@ public:
     int maxSubarrayLength(vector<int>& nums, int k) {
         int n = nums.size();
         int l = 0 , r = 0 , len = 0;
+        int charWithfreqMorethanK = 0;
         unordered_map<int , int> freqMap;
+        //Rather than exploring all the good subarrays , as we only need longest good subarray , we can just keep track of longest good subarray with exploring all good subarray
         while(r < n){
             freqMap[nums[r]] += 1;
-            //Whenever current window has elements greater than frequency of k , shrink it
-            while(freqMap[nums[r]] > k){
+            //Whenever char occurs more than k times in the window , mark it
+            if(freqMap[nums[r]] == k + 1) charWithfreqMorethanK += 1;
+            //Shrink the window , if it contains bad element , anyway we trim by 1 step , therefore maintaining the previous good subarray's length , if it becomes good , then we can expand 
+            if(charWithfreqMorethanK){
                 freqMap[nums[l]] -= 1;
+                if(freqMap[nums[l]] == k) charWithfreqMorethanK -= 1;
                 l += 1;
             }
-            //Calculate the length 
-            len = max(len , r - l + 1);
             r += 1;
         }
-        return len;
-        //TC : O(2N)
+        return n - l;
+        //TC : O(N)
         //SC : O(N) -> Frequency array
+        
     }
 };
