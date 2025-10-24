a2986214c86ae7d204a6e9450068f22691ecb84b
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-10-24 21:52:09 +0530

    Time: 3 ms (83.85%) | Memory: 10.6 MB (24.29%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,21 +1,26 @@
 class Solution {
+private:
+    string helper(string s){
+        string temp = "";
+        int j = 0 , size = s.size();
+        while(j < size){
+            int count = 1;
+            char ch = s[j];
+            while(j < size && s[j] == s[j + 1]){
+                count += 1;
+                j += 1;
+            }
+            temp += to_string(count) + ch;
+            j += 1;
+        }
+        return temp;
+    } 
 public:
     string countAndSay(int n) {
         string ans = "1";
+        //Just simulate , what's given there iteratively!!!
         for(int i = 2 ;  i <= n ; i++){
-            string temp = "";
-            int j = 0 , size = ans.size();
-            while(j < size){
-                int count = 1;
-                char ch = ans[j];
-                while(j < size && ans[j] == ans[j + 1]){
-                    count += 1;
-                    j += 1;
-                }
-                temp += to_string(count) + ch;
-                j += 1;
-            }
-            ans = temp;
+            ans = helper(ans);
         }
         return ans;
     }
