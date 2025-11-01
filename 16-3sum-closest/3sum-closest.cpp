4dce2084e2bc36189aea94e558ede1fd95c62063
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-11-01 10:57:22 +0530

    Time: 722 ms (5.03%) | Memory: 14 MB (27.96%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -5,24 +5,20 @@ public:
         int ans = 1e9;
         for(int i = 0 ; i < n  ; i++){
             for(int j = i + 1 ; j < n ; j++){
-                //bool flag = false;
                 for(int k = j + 1 ; k < n ; k++){
                     int sum = nums[i] + nums[j] + nums[k];
                     if(sum == target) return target;
-                    if(sum > target){
+                    if(abs(sum - target) < abs(ans - target)) ans = sum;
+                    /*if(sum > target){
                         if(sum - target < abs(ans - target)){
                             ans = sum;
-                            //flag = true;
                         }
                     }
                     else{
                         if(target - sum < abs(ans - target) ){
                             ans = sum;
-                            //flag = false;
                         }
-                    }
-                    //if(sum > target && (ans == 1e9 || sum < ans)) ans = sum;
-                    //else if(sum < target && (ans == 1e9 || sum > ans)) ans = sum;
+                    }*/
                 }
             }
         }
