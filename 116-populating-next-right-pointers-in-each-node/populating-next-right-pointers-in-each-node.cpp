835322195826b999562c105ca21b2a104ebbde49
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-11-07 11:42:59 +0530

    Time: 14 ms (53.62%) | Memory: 19.1 MB (69.49%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -40,6 +40,7 @@ public:
     Node* connect(Node* root) {
         if(!root) return root;
         if(root->left) root->left->next = root->right;
+        //To connect right node's next to left node of other subtree , we're making use of next link of root node
         if(root->right && root->next) root->right->next = root->next->left;
         connect(root->left);
         connect(root->right);
