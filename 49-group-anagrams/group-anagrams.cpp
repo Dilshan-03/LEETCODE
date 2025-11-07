3179bc9646e9b85ceb796310c2d6570ba3be1113
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-11-07 11:03:49 +0530

    Time: 21 ms (37.63%) | Memory: 26.6 MB (29.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string , vector<int>> hashMap;//{ sorted string , {indices}}
         int n = strs.size();
-        vector<string> temp(strs);
-        for(int i = 0 ; i < n ; i++)
-           sort(temp[i].begin() , temp[i].end());
-        int i = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            string temp = strs[i];
+            //To reduce complexity , since string contains only lowercase letters , we can use counting sort approach too
+            sort(temp.begin() , temp.end());
+            hashMap[temp].push_back(i); //store the anagrams with indices
+        }
+
         vector<vector<string>> result;
-        vector<int> visited(n , 0);
-        while(i < n){
-            if(visited[i]){
-                i += 1;
-                continue;
+        for(auto it : hashMap){
+            vector<string> anagrams;
+            for(int i : it.second){
+                anagrams.push_back(strs[i]);
             }
-            visited[i] = 1;
-            vector<string> curAnagram;
-            curAnagram.push_back(strs[i]);
-            for(int j = i + 1 ; j < n ; j++){
-                if(temp[i] == temp[j]){
-                    curAnagram.push_back(strs[j]);
-                    visited[j] = 1;
-                }
-            }
-            result.push_back(curAnagram);
-            i += 1;
+            result.push_back(anagrams);
         }
         return result;
+
+        //TC : O(N * M log M) + O(N) -> N (number of string in strs) , M (avg length of each string)
+        //SC : O(N + N) + O(N)(result)
     }
 };
