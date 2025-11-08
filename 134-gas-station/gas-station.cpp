e32a2cf3afa25a2d5fe9cfcf817f38b48b736f99
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-11-08 13:37:32 +0530

    Time: 8 ms (7.84%) | Memory: 112.6 MB (23.13%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -4,15 +4,17 @@ public:
         int n = gas.size();
         int totalGas = accumulate(gas.begin() , gas.end() , 0);
         int totalCost = accumulate(cost.begin() , cost.end() , 0);
+        //Edge case -> if total gas that can be accumulated is less than cost for travelling , then definitely , it's not possible
         if(totalCost > totalGas) return -1;
         int total = 0;
         int start = 0;
         //Greedy strategy
         for(int i = 0 ; i < n ; i++){
             total += (gas[i] - cost[i]);
+            //If the current sum is negative , then surely . points from start to current index(i) will not be starting index --> Greedy strategy
             if(total < 0){
                 total = 0;
-                start = i + 1;
+                start = i + 1;//Reason for not exploring all stations from start , question guarantees that unique solution is always present
             }
         }
         return start;
