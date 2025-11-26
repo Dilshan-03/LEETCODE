f8e35d46f5e90e56464e4d7fbf6e1fa8278d755f
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-11-26 11:27:28 +0530

    Time: 299 ms (23.22%) | Memory: 140.5 MB (34.83%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -4,17 +4,29 @@ private:
     int helper(int row , int col , int sum , vector<vector<int>>& grid , vector<vector<vector<int>>>& dp ,  int k , int n , int m){
         if(row == n || col == m) return 0;
         if(row == n - 1 && col == m - 1) return ((sum + grid[row][col]) % k) ? 0 : 1 ; 
-        sum = sum + grid[row][col];
-        if(dp[row][col][sum % k] != -1) return dp[row][col][sum % k]; 
-        int down = helper(row + 1 , col , sum , grid , dp , k , n , m );
-        int right = helper(row , col + 1 , sum , grid , dp ,  k , n , m);
-        return dp[row][col][sum % k] = (down + right) % mod;
+        if(dp[row][col][(sum + grid[row][col]) % k] != -1) return dp[row][col][(sum + grid[row][col]) % k]; 
+        int down = helper(row + 1 , col , sum + grid[row][col], grid , dp , k , n , m );
+        int right = helper(row , col + 1 , sum + grid[row][col], grid , dp ,  k , n , m);
+        return dp[row][col][(sum + grid[row][col]) % k] = (down + right) % mod;
     }
 public:
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>>dp(n , vector<vector<int>>(m , vector<int>(k , -1)));
+        vector<vector<vector<int>>>dp(n  , vector<vector<int>>(m , vector<int>(k , -1)));
         return helper(0 , 0 , 0 , grid, dp , k , n , m);
+       
+
+       /* for(int row = n - 2 ; row >= 0 ; row --){
+            for(int col = m - 1 ; col >= 0 ; col--){
+                for(int sum = 0 ; sum < k ; sum++){
+                    int down = dp[row + 1][col][(sum + grid[row][col]) % k];
+                    int right = dp[row][col + 1][(sum + grid[row][col]) % k];
+                    dp[row][col][(sum + grid[row][col])% k] = (down + right) % mod;
+                }
+            }
+        }
+        return dp[0][0][0];*/
+        
     }
 };
