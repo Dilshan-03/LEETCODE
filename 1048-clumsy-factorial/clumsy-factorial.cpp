222d6dfc250e5e583604f575409b63616fb66e26
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-12-10 21:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (89.57%) - LeetSync

--- 1048-clumsy-factorial/clumsy-factorial.cpp
+++ 1048-clumsy-factorial/clumsy-factorial.cpp
@@ -6,16 +6,19 @@ public:
             if(choice == 3) ans1 *= i;
             else if(choice == 2) ans1 /= i;
             else if(choice == 1){
+                //Do the operation of minus who had ignored once
                 if(ans2) ans1 = ans2 - ans1;
                 ans1 += i;
                 ans2 = 0;
             }
             else{
+                //Ignore the operation , there may be a chance for multiplication subsequently
                 ans2 = ans1;
                 ans1 = i;
                 choice = 4;
             }
         }
+        //If There's still a term , that needs subtraction to be performed , do it
         if(ans2) return ans2 - ans1;
         return ans1;
     }
