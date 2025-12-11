cf8af1a3779ab8c337ce880cb8f060ec66087626
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-12-11 22:35:24 +0530

    Time: 348 ms (66.85%) | Memory: 362.2 MB (58.01%) - LeetSync

---
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        vector<int> rowMax(n + 1 , 0) , rowMin(n + 1 , n + 1);
+        vector<int> colMax(n + 1 , 0) , colMin(n + 1 , n + 1);
+        for(auto it : buildings){
+            int x = it[0];
+            int y = it[1];
+            //Let's say (i , j) , here j value is same for all same row elements and i value is same for all same column elements
+            rowMax[y] = max(rowMax[y] , x);
+            rowMin[y] = min(rowMin[y] , x);
+            colMax[x] = max(colMax[x] , y);
+            colMin[x] = min(colMin[x] , y);
+        }
+        int cnt = 0;
+        for(auto it : buildings){
+            int x = it[0] , y = it[1];
+            if((rowMin[y] < x && x < rowMax[y]) && (colMin[x] < y && y <  colMax[x])) cnt += 1;
+        }
+        return cnt;
+    }
+};
