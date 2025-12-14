b1cb5fbbdd71974242d517359f3e8aff6d3009f7
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-12-14 12:06:21 +0530

    Time: 1082 ms (11.00%) | Memory: 399 MB (9.50%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -19,7 +19,22 @@ private:
 public:
     int numberOfWays(string corridor) {
         int n = corridor.length();
-        vector<vector<int>> dp(n , vector<int>(3 , -1));
-        return f(0 , 0 , n , corridor , dp);
+        vector<vector<int>> dp(n + 1 , vector<int>(4 , 0));
+        //return f(0 , 0 , n , corridor , dp);
+        dp[n][2] = 1;
+        for(int i = n - 1 ; i >= 0 ; i--){
+            for(int cnt = 2 ; cnt >= 0 ; cnt--){
+              if(corridor[i] == 'S'){
+                if(cnt + 1 == 2) dp[i][cnt] = (dp[i + 1][0] + dp[i + 1][cnt + 1]) % mod;
+                else if(cnt + 1 < 2) dp[i][cnt] = dp[i + 1][cnt + 1] % mod;
+                else dp[i][cnt] = 0;
+              }
+              else{
+                if(cnt == 2) dp[i][cnt] = (dp[i + 1][0] + dp[i + 1][cnt]) % mod;
+                else  dp[i][cnt] = dp[i + 1][cnt] % mod;
+               }
+            }
+        }
+    return dp[0][0];
     }
 };
