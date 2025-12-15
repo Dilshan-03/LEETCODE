450bbdb30859229e9fd23c634fac8bc9eb5314c1
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-12-15 10:51:18 +0530

    Time: 4 ms (30.48%) | Memory: 106.9 MB (97.03%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
@@ -6,7 +6,9 @@ public:
         long long cnt = 0;
         while(r < n){
             cnt += (r - l + 1);
+            //Increase the window size by 1 and check for valid window
             r += 1;
+            //If Window is not valid , then reduce the window size to single element
             if(r < n && prices[r] + 1 != prices[r - 1]) l = r;
         }
         return cnt;
