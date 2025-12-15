1a5a4980aff9a43d59d8e80ebbe4c08352af261b
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2025-12-15 14:16:56 +0530

    Time: 0 ms (100.00%) | Memory: 65.2 MB (65.70%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -6,11 +6,14 @@ public:
         int n = nums.size();
         int cnt = 0;
         while(r < n){
+            //Take the current element into the window
             prod *= nums[r];
+            //Shrink the window if it's invalid
             while(l <= r && prod >= k){
                 prod /= nums[l];
                 l += 1;
             }
+            //Add the cnt of valid windows
             cnt += (r - l + 1);
             r += 1;
         }
