14dc78995894e5234b8b4c7b4ef6348a3ea6d0ba
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2026-01-07 14:57:27 +0530

    Time: 2 ms (77.46%) | Memory: 78.8 MB (39.67%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -19,19 +19,20 @@ class Solution {
         sum += findTotalSum(root->right);
         return sum;
     }
-    long long helper(TreeNode* root , long long& maxProduct , long long total){
+    long long helper(TreeNode* root , long long& maxProduct , long long totalSum){
         if(!root) return 0;
-        long long sum = 0;
-        sum += root->val;
-        sum += helper(root->left , maxProduct , total);
-        sum += helper(root->right , maxProduct , total);
-        long long rem = total - sum;
-        long long curProduct = sum * rem;
+        long long curSubtreeSum = 0;
+        curSubtreeSum += root->val;
+        curSubtreeSum += helper(root->left , maxProduct , totalSum);
+        curSubtreeSum += helper(root->right , maxProduct , totalSum);
+        long long remSum = totalSum - curSubtreeSum;
+        long long curProduct = curSubtreeSum * remSum;
         maxProduct = max(maxProduct  , curProduct);
-        return sum;
+        return curSubtreeSum;
     }
 public:
     int maxProduct(TreeNode* root) {
+        //Calculate the total sum
         long long total = findTotalSum(root);
         long long max = 0;
         helper(root , max ,  total);
