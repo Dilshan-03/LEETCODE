8b7b21fa7e2e3c88c40ee582e23bd73270a4731e
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2026-01-10 19:14:30 +0530

    Time: 21 ms (40.41%) | Memory: 18.4 MB (23.97%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -18,9 +18,28 @@ private:
 public:
     int minimumDeleteSum(string s1, string s2) {
         int n = s1.length() , m = s2.length();
-        vector<vector<int>> dp(n + 1  , vector<int>(m + 1 , -1));
+        vector<vector<int>> dp(n + 1  , vector<int>(m + 1 , 0));
 
-        return f(n  , m  , s1 , s2 , dp);
+        //Base case for i == 0
+        for(int j = 1 ; j <= m ; j++){
+            int totalAscii = 0;
+            for(int k = 0 ; k < j ; k++) totalAscii += s2[k];
+            dp[0][j] = totalAscii;
+        }
+        //Base case for j == 0
+        for(int i = 1 ; i <= n ; i++){
+            int totalAscii = 0;
+            for(int k = 0 ; k < i ; k++) totalAscii += s1[k];
+            dp[i][0] = totalAscii;
+        }
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = min(s1[i - 1] + dp[i - 1][j] , s2[j - 1] + dp[i][j - 1]);
+            }
+        }
+        return dp[n][m];
 
     }
 };
