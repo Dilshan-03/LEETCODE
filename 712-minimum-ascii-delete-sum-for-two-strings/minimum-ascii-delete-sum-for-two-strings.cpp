7f3e59af791dd183fda56add87ba16d7bde23c96
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2026-01-10 18:59:17 +0530

    Time: 35 ms (28.95%) | Memory: 18.4 MB (23.97%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int f(int i , int j , string& s1 , string&s2 , vector<vector<int>>& dp){
+        if(i < 0){
+            int totalAscii = 0;
+            for(int k = 0 ; k <= j ; k++) totalAscii += s2[k];
+            return totalAscii;
+        }
+        if(j < 0){
+            int totalAscii = 0;
+            for(int k = 0 ; k <= i ; k++) totalAscii += s1[k];
+            return totalAscii;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1[i] == s2[j]) return dp[i][j] = f(i - 1 , j - 1 , s1 , s2 , dp);
+        return dp[i][j] = min(s1[i] + f(i - 1 , j, s1 , s2 , dp) , s2[j] + f(i , j - 1 , s1 , s2 , dp));
+    }
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int n = s1.length() , m = s2.length();
+        vector<vector<int>> dp(n , vector<int>(m , -1));
+        return f(n - 1 , m - 1 , s1 , s2 , dp);
+
+    }
+};
