166162af0edc11f03e55ac31d06b38d62033ab7e
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2026-01-24 22:25:17 +0530

    Time: 168 ms (70.60%) | Memory: 100 MB (30.18%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -1,24 +1,17 @@
 class Solution {
-private:
-    bool isPossibleSum(vector<int>& nums , int maxVal , int n){
-        int l = 0 , r = n - 1;
-        while(l < r){
-            if(nums[l] + nums[r] > maxVal) return false;
-            l += 1;
-            r -= 1;
-        }
-        return true;
-    }
 public:
     int minPairSum(vector<int>& nums) {
         sort(nums.begin() , nums.end());
         int n = nums.size();
-        int low = 1 , high = (nums[n - 1] + nums[n - 2]);
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(isPossibleSum(nums , mid , n)) high = mid - 1;
-            else low = mid + 1;
+        int minMaxSum = 0;
+        int l = 0 , r = n - 1;
+        while(l < r){
+            int sum = nums[l] + nums[r];
+            minMaxSum = max(minMaxSum , sum);
+            l += 1;
+            r -= 1;
         }
-        return low;
+        return minMaxSum;
+        
     }
 };
