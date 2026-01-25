e3c1f061ee3ff4475f7aa61d3bd3996ca17a0b82
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2026-01-25 12:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (56.92%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin() , nums.end());
         int mini = INT_MAX;
         int n = nums.size();
-        //Create a window and check for all possible differences
+        //Create a window of size k and check for all possible differences
         for(int i = 0 ; i <= n - k ; i++){
             int low = nums[i];
             int high =  nums[i + k - 1];
