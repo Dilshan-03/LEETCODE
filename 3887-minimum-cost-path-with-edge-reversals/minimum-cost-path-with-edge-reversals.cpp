d9ba4d6ac6b4f210d3e16133313947f5ee28d116
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2026-01-27 22:49:37 +0530

    Time: 235 ms (75.00%) | Memory: 290.4 MB (76.20%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
@@ -3,12 +3,13 @@ public:
     int minCost(int n, vector<vector<int>>& edges) {
         vector<int> dist(n , 1e9);
         vector<vector<pair<int , int>>> adjList(n);
-        //Construct adjList
+        //Construct adjList by also including reverse edge for each u , with weight of 2 * w
         for(int i = 0 ; i < edges.size() ; i++){
             int u = edges[i][0] , v = edges[i][1] , wt = edges[i][2];
             adjList[u].push_back({v , wt});
             adjList[v].push_back({u , 2 * wt});
         }
+        //Typical Dijkstra's algo 
         priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int , int>>> minHeap;
         minHeap.push({0 , 0});
         dist[0] = 0;
