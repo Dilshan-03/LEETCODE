ac357df6f84afb5dfebeb38ac914aa50fde2d2b7
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2026-02-01 13:19:18 +0530

    Time: 0 ms (100.00%) | Memory: 30.6 MB (48.24%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -3,6 +3,7 @@ public:
     int minimumCost(vector<int>& nums) {
         int firstMin = 1e9 , secondMin = 1e9;
         int n = nums.size();
+        //Finding 1 st and 2 nd minimum numbers from the array excluding 1 st element of array
         for(int i = 1 ; i < n ; i++){
             if(nums[i] < firstMin){
                 secondMin = firstMin;
@@ -10,6 +11,7 @@ public:
             }
             else if(nums[i] < secondMin) secondMin = nums[i];
         }
+        //Since subarry is asked , nums[0] can surely be the first element of 1 subarray and accordingly other minimum elements can be taken as other 2 subarrays first element
         return (nums[0] + firstMin + secondMin);
     }
 };
