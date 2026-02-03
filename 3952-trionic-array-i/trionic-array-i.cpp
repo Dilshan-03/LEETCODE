376e407c5b61fcdb18d98b09890a4b48a3d4de2f
Dilshan Chinnappan A <dilsandilsan2006@gmail.com>
2026-02-03 19:34:24 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (35.38%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -3,20 +3,22 @@ public:
     bool isTrionic(vector<int>& nums) {
         int p = -1 , q = -1 , n = nums.size();
         int i = 1;
+        //Looking for p - value in first strictly increasing half
         while(i < n - 1 && nums[i - 1] < nums[i]){
             p = i ; 
             i += 1;
         }
         if(nums[i] == nums[i - 1]) return false;
+        //Looking for q - value in second strictly decreasing half
         while(i < n - 1 && nums[i - 1] > nums[i]){
             q = i;
             i += 1;
         }
         if(nums[i] == nums[i - 1]) return false;
-
+        //Checking whether third half is strictly increasing half
         while(i < n && nums[i - 1] < nums[i]) i += 1;
-        if(i < n) return false;
-        if(p != -1 && q != -1) return true;
+        
+        if(i == n && p != -1 && q != -1) return true;
         return false;
     }
 };
